522af11f644b0e47157c3e5a00cb063f5d3699d1
Tulendra Chandrakar <98647158+Tulendra1111@users.noreply.github.com>
2025-08-11 19:07:33 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -33,7 +33,7 @@ const HeroSection: React.FC = () => {
         {/* Logo/Profile Section */}
         <div className="flex justify-center mb-8">
           <div className="relative">
-            <img src="public\MyPhoto.png" alt="TC image" className="w-24 h-24 animate-glow" />
+            <img src="public\MyPhoto.jpg" alt="TC image" className="w-24 h-24 animate-glow" />
             <div className="absolute -bottom-2 -right-2 w-8 h-8 bg-green-500 rounded-full border-4 border-portfolio-dark flex items-center justify-center">
               <div className="w-2 h-2 bg-white rounded-full"></div>
             </div>
